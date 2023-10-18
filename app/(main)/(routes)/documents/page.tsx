0c66868c20ad81d2ab81ad9900c1172d611cc161
aarsh21 <aarshpersonal21@gmail.com>
2023-10-18 21:29:11 +0530

    feat: dropDownMenu to sidebar and ability to log out from there.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,36 @@
-"use-client"
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      <p>docuements page</p>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Slotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        create a note
+      </Button>
     </div>
   );
 };
